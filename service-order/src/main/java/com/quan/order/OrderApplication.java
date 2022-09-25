72416dc18b358fa27b7e4087b093e056893bc904
Quan <Hilda_quan@163.com>
2022-09-25 20:36:55 +0800

    update

--- service-order/src/main/java/com/quan/order/OrderApplication.java
+++ service-order/src/main/java/com/quan/order/OrderApplication.java
@@ -1,2 +1,13 @@
-package com.quan.order;public class OrderApplication {
+package com.quan.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 }
